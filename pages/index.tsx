85090a847f8dd54c49c0a1a2ff2fcc5891ad0f80
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2024-06-12 15:29:54 +0100

    Infinite scroll of posts (#6)
    * Create a simple scroll of posts
    
    In this feature, I used Posts Placeholder Api which  render a simple scroll of different posts.
    
    * Setup the prettier and made file changes
    
    In this change, I set up prettier in the project and move the function types outside the Reactjs function
    
    * Create an infinite scroll of posts
    
    In this change, I created a Posts.tsx file that can allow infinite scroll of posts with loading when navigatimg down the posts.
    
    * Create an infinite scroll of posts
    
    In this change, I created a Posts.tsx file that can allow infinite scroll of posts with loading when navigatimg down the posts.
    
    * Fix the infinite scroll of posts
    
    In this change I fixed the changes based on the review to have an infinite scroll of posts
    
    * optimising next build
    
    In this change I optimised the project by runnning the nex build command to be able to check the vercel preview
    
    * adjust the files
    
    In this change, I removed the posts.tsx and incorporated the rendering of the API inside the index.tsx file to avoid rendering the same thing
    
    * Fix the problem of the loading state
    
    In this change, I made the loading state to render the right jsx elements for loading and no more posts
    
    ---------
    
    Co-authored-by: Motunrayo Adeneye <motunrayodeneye@gmail.comgit config --global user.name Motunrayo>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { inter, roboto_serif } from "../styles/fonts";
 
 //Api
-const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+const POSTS_URL =
+  "https://jsonplaceholder.typicode.com/posts?_per_page=12&_page=";
 
+// Type definitions
 type Post = {
   id: number;
   title: string;
@@ -16,37 +18,64 @@ type Error = {
 };
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   const fetchPosts = async () => {
     try {
-      setLoading(true);
-      const response = await fetch(POSTS_URL);
+      const response = await fetch(`${POSTS_URL}${page}`);
       if (!response.ok) {
         throw new Error("Failed to fetch posts");
       }
       const posts = await response.json();
-      setPosts(posts);
+      setPosts((prev) => [...prev, ...posts]);
+      if (posts.length > 10) {
+        setHasMore(true);
+      } else if (posts.length === 0) {
+        setHasMore(true);
+      } else {
+        setHasMore(false);
+      }
     } catch (error: any) {
       setError(error.message);
     } finally {
       setLoading(false);
     }
   };
+  useEffect(() => {
+    setTimeout(() => {
+      fetchPosts();
+    }, 1000);
+  }, [page]);
 
   useEffect(() => {
-    fetchPosts();
-  }, []);
+    const handleScroll = () => {
+      console.log("top", document.body.scrollTop);
+      console.log("height", document.body.scrollHeight);
+      if (
+        window.innerHeight + document.body.scrollTop + 1 >=
+        document.body.scrollHeight
+      ) {
+      }
+      if (!hasMore) {
+        setPage((prev) => prev + 1);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
 
-  if (loading) {
-    return (
-      <div className="flex justify-center min-h-screen items-center flex-col text-[var(--main-color)]">
-        <p className="text-[1.85rem]">Loading Posts...</p>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="flex justify-center min-h-screen items-center flex-col text-[var(--main-color)]">
+  //       <p className="text-[1.85rem]">Loading Posts...</p>
+  //     </div>
+  //   );
+  // }
   if (error) {
     return (
       <div className="flex justify-center items-center flex-col text-[var(--main-color)]">
@@ -56,23 +85,39 @@ export default function Home() {
   }
 
   return (
-    <div className="flex justify-center items-center flex-col bg-[var(--main-bg-color-default)] text-[var(--main-color)] ">
+    <div
+      className="flex justify-center items-center flex-col bg-[var(--main-bg-color-default)] text-[var(--main-color)] py-4"
+      ref={scrollContainerRef}
+    >
       <h1 className={`${inter.className} text-2xl py-4 font-bold`}>
         Post Feed
       </h1>
       <div className="grid gap-4 max-w-[700px] w-calc[100% - 2rem] max-sm:max-w-[325px]">
-        {posts.map((post: Post) => {
-          return (
-            <div
-              key={post.id}
-              className={`${roboto_serif.className} text-[.85rem] bg-[var(--main-bg-color)] p-4 border border-[var(--main-border)]`}
-            >
-              <h2 className="font-bold mb-2">{post.title}</h2>
-              <p className="">{post.body}</p>
-            </div>
-          );
-        })}
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post: Post) => {
+            return (
+              <div
+                key={post.id}
+                className={`${roboto_serif.className} text-[.85rem] bg-[var(--main-bg-color)] p-4 border border-[var(--main-border)]`}
+              >
+                <h2 className="font-bold mb-2">
+                  {post.id} {post.title}
+                </h2>
+                <p className="">{post.body}</p>
+              </div>
+            );
+          })}
       </div>
+      {!hasMore ? (
+        <div className="py-4">
+          <p className="text-[1rem] text-[var(--main-color)] ">Loading...</p>
+        </div>
+      ) : (
+        <div className="py-4">
+          <p className="text-[1rem] text-[var(--main-color)] ">No more posts</p>
+        </div>
+      )}
     </div>
   );
 }
